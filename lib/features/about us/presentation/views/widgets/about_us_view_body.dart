e70adedce8d7a5ead8737f7b5beb12c41a8efe07
HazemAttia7 <lolhazem41@gmail.com>
2026-02-19 22:45:09 +0200

    feat(about-us) : implement slogan section

--- lib/features/about us/presentation/views/widgets/about_us_view_body.dart
+++ lib/features/about us/presentation/views/widgets/about_us_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:poem_app/core/helper/constants.dart';
+import 'package:poem_app/features/about%20us/presentation/views/widgets/slogan_section.dart';
 import 'package:poem_app/features/home/presentation/views/widgets/page_header.dart';
 
 class AboutUsViewBody extends StatelessWidget {
@@ -8,14 +10,18 @@ class AboutUsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: kHorizontalPagePadding),
-        child: const Column(
-          children: [
-            // TODO : Implement About Us view
-            PageHeader(subTitle: 'من نحن'),
-            Expanded(child: Center(child: Text("صفحة من نحن"))),
-          ],
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: kHorizontalPagePadding),
+          child: Column(
+            children: [
+              Gap(MediaQuery.sizeOf(context).height * 0.022),
+              const PageHeader(subTitle: 'من نحن'),
+              Gap(MediaQuery.sizeOf(context).height * 0.035),
+              const SolganSection(),
+              Gap(MediaQuery.sizeOf(context).height * 0.03),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/features/about us/presentation/views/widgets/slogan_section.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:poem_app/core/utils/app_styles.dart';
+
+class SolganSection extends StatelessWidget {
+  const SolganSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          '"موسوعة الشعر العربي في يدك"',
+          style: context.textStyle20.copyWith(
+            fontFamily: GoogleFonts.notoNastaliqUrdu().fontFamily,
+          ),
+        ),
+        Gap(MediaQuery.sizeOf(context).height * 0.03),
+        Text.rich(
+          textAlign: TextAlign.center,
+          TextSpan(
+            style: context.textStyle14.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            children: [
+              TextSpan(
+                text: "ديوان",
+                style: context.textStyle18.copyWith(
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+              const TextSpan(
+                text:
+                    " — تطبيق يجمع روائع الشعر العربي من مختلف العصور، يتيح لك قراءة القصائد وحفظ المفضلة والاستمتاع بها في أي وقت. نسعى إلى تقريب التراث الشعري من الأجيال الجديدة .",
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
